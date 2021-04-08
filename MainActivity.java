3e20326ec8a9437a52191d642aa6c862c4732780
AroshaMaryam <71967472+AroshaMaryam@users.noreply.github.com>
2021-04-08 23:31:56 +0000

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.todotasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> toDoList;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toDoList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this,R.layout.list_view_layout,toDoList);
+        listView = findViewById(R.id.id_list_view);
+
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            TextView textView = (TextView) view;
+            textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        });
+        editText =findViewById(R.id.id_edit_text);
+
+    }
+    public void addItemToList(View view) {
+        toDoList.add(editText.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+
+        editText.setText("");
+    }
+}
